9fff374d24e96a35ca4f9d684883c164ec37d187
Mike Rombout <mike.rombout1@gmail.com>
2015-11-22 15:30:14 +0100

    Add unit tests for musical tokens

--- lexer/test/matcher/CharMatcherTest.cpp
+++ lexer/test/matcher/CharMatcherTest.cpp
@@ -8,13 +8,14 @@ protected:
 
     }
 
+    std::vector<dem::lexer::Token> tokens;
     dem::lexer::CharMatcher matcher;
 };
 
 TEST_F(CharMatcherTest, ReturnsEmptyWhenNoMatch) {
     std::string str{"bcdefa"};
 
-    std::string result = matcher.match(str.begin(), str.end());
+    std::string result = matcher.match(str.begin(), str.end(), tokens);
 
     ASSERT_EQ("", result);
 }
@@ -22,7 +23,7 @@ TEST_F(CharMatcherTest, ReturnsEmptyWhenNoMatch) {
 TEST_F(CharMatcherTest, ReturnsCharWhenMatch) {
     std::string str{"abcde"};
 
-    std::string result = matcher.match(str.begin(), str.end());
+    std::string result = matcher.match(str.begin(), str.end(), tokens);
 
     ASSERT_EQ("a", result);
 }
@@ -30,7 +31,7 @@ TEST_F(CharMatcherTest, ReturnsCharWhenMatch) {
 TEST_F(CharMatcherTest, ReturnsEmptyWithEndIterator) {
     std::string str{"abcde"};
 
-    std::string result = matcher.match(str.end(), str.end());
+    std::string result = matcher.match(str.end(), str.end(), tokens);
 
     ASSERT_EQ("", result);
 }
